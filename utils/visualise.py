639dfc26ba2ec1900f4c400235885189b0fea843
Suchita Kulkarni <suchita.kulkarni@gmail.com>
2026-03-30 18:02:46 +0200

    Update visualise.py

--- utils/visualise.py
+++ utils/visualise.py
@@ -28,8 +28,12 @@ from scipy.stats import gaussian_kde, mannwhitneyu
 
 from pathlib import Path
 
-root_path = Path(os.environ["PROJECT_ROOT"])
-
+if "PROJECT_ROOT" in os.environ:
+    root_path = Path(os.environ["PROJECT_ROOT"]).resolve()
+else:
+    # fallback: assume this file is somewhere inside src/
+    root_path = Path(__file__).resolve().parents[1]
+    
 if str(root_path) not in sys.path:
     sys.path.insert(0, str(root_path))
 
