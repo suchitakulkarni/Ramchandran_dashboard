9e6403901d3846a8c888b560edf52f3676b7f09e
Suchita Kulkarni <suchita.kulkarni@gmail.com>
2026-03-30 18:01:16 +0200

    Update train_vae.py

--- src/train_vae.py
+++ src/train_vae.py
@@ -10,7 +10,12 @@ from sklearn.preprocessing import MinMaxScaler
 import joblib
 
 from pathlib import Path
-root_path = Path(os.environ["PROJECT_ROOT"])
+
+if "PROJECT_ROOT" in os.environ:
+    root_path = Path(os.environ["PROJECT_ROOT"]).resolve()
+else:
+    # fallback: assume this file is somewhere inside src/
+    root_path = Path(__file__).resolve().parents[1]
 if str(root_path) not in sys.path:
     sys.path.insert(0, str(root_path))
 
